99d231fa120ac6e54ecff8728cc90a2929910dde
itjob <35135746+ITJOBTRAINERS@users.noreply.github.com>
2024-04-02 11:23:39 +0530

    Delete calculator-pr directory

--- calculator-pr/calculator-pr/src/components/Display.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Display.module.css"
-
-//destructuring displayValue, else [Object Object] error wil get
-const Display=({displayValue})=>{
-    return(
-        <input className={styles.display} value={displayValue}type="text" readOnly/>
-    )    
-}
-export default Display
